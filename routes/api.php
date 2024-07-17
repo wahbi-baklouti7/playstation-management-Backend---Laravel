6ac0ac5ef02ef713061986c76f44d7189176eafc
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2024-07-17 14:14:47 +0100

    Implement authentication feature

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\AuthenticationController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,5 +18,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return 1;
     return $request->user();
 });
+
+
+// ===================== Begin Users Routes =====================
+
+Route::get('/users',[UserController::class,'index']);
+Route::get('/users/{phpid}',[UserController::class,'show']);
+// ===================== End Users Routes =====================
+
+Route::post('/login',[AuthenticationController::class,'login']);
+Route::post('/register',[AuthenticationController::class,'register']);
+Route::post('/logout',[AuthenticationController::class ,'logout'])->middleware('auth:sanctum');
+
+// Route::get('/', function () {
+
+//     return 'test';
+// });
